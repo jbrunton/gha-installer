df0a4d836a32a52977d178db53ff04f359f25444
John Brunton <john_brunton@live.co.uk>
2020-08-07 16:27:34 +0100

    add verification scripts

--- scripts/check_clean_repo.js
+++
@@ -1,6 +0,0 @@
-const shell = require('shelljs')
-
-if (shell.exec('git diff --exit-code HEAD', { silent: true }).code != 0) {
-  console.log('Code changes found, commit your changes')
-  process.exit(1)
-}
