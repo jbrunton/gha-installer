33a2cb895c5a235dfc7eca2aa817c6f9d166fb4a
John Brunton <john_brunton@live.co.uk>
2020-08-07 15:29:13 +0100

    convert single-app example to typescript

--- examples/single-app/index.ts
+++ examples/single-app/index.ts
@@ -1,21 +1,21 @@
-const {Installer, GitHubReleasesService} = require('@jbrunton/gha-installer')
-const core = require('@actions/core')
-const github = require('@actions/github')
+import {Installer, GitHubReleasesService, Octokit} from '@jbrunton/gha-installer'
+import * as core from '@actions/core'
+import * as github from '@actions/github'
 
-async function run() {
+async function run(): Promise<void> {
   try {
     const octokit = createOctokit()
     const repo = { owner: 'jbrunton', repo: 'gflows' }
     const releasesService = GitHubReleasesService.create(octokit, repo, 'gflows-linux-amd64')
     const installer = Installer.create(releasesService)
 
-    return await installer.installApp({ name: 'gflows', version: core.getInput('version') })
+    return await installer.installApp({ name: 'gflows', version: '0.1.0' })
   } catch (error) {
     core.setFailed(error.message)
   }
 }
 
-function createOctokit() {
+function createOctokit(): Octokit {
   const token = core.getInput('token');
   return github.getOctokit(token);
 }
