e0d4259dec8c32dcb49b76c1751fe05ea41dd6a7
John Brunton <john_brunton@live.co.uk>
2020-08-07 14:39:05 +0100

    add multi-app example

---
+++ examples/multi-app-multi-platform/index.js
@@ -0,0 +1,41 @@
+const {Installer, GitHubReleasesService} = require('@jbrunton/gha-installer')
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+async function run() {
+  try {
+    const octokit = createOctokit()
+    const releasesService = GitHubReleasesService.create(octokit, getRepo, getAssetName)
+    const installer = Installer.create(releasesService)
+
+    return await installer.installAll(getAppsToDownload())
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+function createOctokit() {
+  const token = core.getInput('token');
+  return github.getOctokit(token);
+}
+
+function getRepo(app) {
+  return { owner: 'k14s', repo: app.name }
+}
+
+function getAssetName(app) {
+  switch (process.platform) {
+    case 'win32': return `${app.name}-windows-amd64.exe`
+    case 'darwin': return `${app.name}-darwin-amd64`
+    default: return `${app.name}-linux-amd64`
+  }
+}
+
+function getAppsToDownload() {
+  const k14sApps = ['ytt', 'kbld', 'kapp', 'kwt', 'imgpkg', 'vendir']
+  return k14sApps.map(app => {
+    return { app: app, version: core.getInput(app) }
+  })
+}
+
+run()
